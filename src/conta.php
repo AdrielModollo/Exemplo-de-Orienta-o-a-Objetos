a689a95925cb3b1696dc8947f4fb27fb462f6808
Adriel Modollo <adrielmodollo@outlook.com>
2020-09-03 12:31:39 -0300

    Adicionad funcao para deposito e transferencia

--- src/conta.php
+++ src/conta.php
@@ -15,15 +15,37 @@ class Conta
 {
     public string $cpfTitular;
     public string $nomeTitular;
-    public float $saldo;
+    public float $saldo = 0;
     
     public function sacar(float $valorASacar)
     {
         if ($valorASacar > $this->saldo) {
             echo "Saldo indisponível";
-        } else {
+            return;
+        } 
             $this->saldo -= $valorASacar;
+        
+    }
+
+    public function depositar(float $valorADepositar): void
+    {
+        if ($valorADepositar < 0) {
+            echo "Valor precisa ser positivo";
+            return;
         }
+    
+        $this->saldo += $valorADepositar;
+    }
+    
+    public function transferir(float $valorATransferir, Conta $contaDestino): void
+    {
+        if ($valorATransferir > $this->saldo) {
+            echo "Saldo indisponível";
+            return;
+        } 
+    
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
     }
 }
 
@@ -35,8 +57,16 @@ $segundaConta->saldo = 300;
 var_dump($umaConta);  
 echo $umaConta->saldo;
 
+
 $umaConta->sacar(50);
 echo $umaConta->saldo;
 /*armazena o endereço da memória
 $c = $segundaConta;
 var_dump($c); */
+
+$umaConta->saldo = 0;
+$umaConta->depositar = 100;
+echo $umaConta->depositar;
+$umaConta->depositar(-100);
+echo $umaConta->depositar;
+
